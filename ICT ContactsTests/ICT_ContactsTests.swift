42a69e8005fc019669c66ab7670d85d72e3133fc
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-03-05 02:26:02 +0600

    Initial Commit

---
+++ ICT ContactsTests/ICT_ContactsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ICT_ContactsTests.swift
+//  ICT ContactsTests
+//
+//  Created by Md Khaled Hasan Manna on 5/3/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import XCTest
+@testable import ICT_Contacts
+
+class ICT_ContactsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
