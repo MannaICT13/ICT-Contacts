aab41fd0a1134d53ea88148339e7251b11d6ee1a
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-03-05 03:07:21 +0600

    Added 4 button and there action and outlet

---
+++ ICT Contacts/Controller/HomeController/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ICT Contacts
+//
+//  Created by Md Khaled Hasan Manna on 5/3/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var studentBtn: UIButton!
+    
+    @IBOutlet weak var teaherBtn: UIButton!
+    
+    @IBOutlet weak var officeStaffBtn: UIButton!
+    
+    @IBOutlet weak var aboutBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        studentBtn.layer.cornerRadius = 10
+        teaherBtn.layer.cornerRadius = 10
+        officeStaffBtn.layer.cornerRadius = 10
+        aboutBtn.layer.cornerRadius = 10
+        
+       
+    }
+
+    @IBAction func studentBtnAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func teacherBtnAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func officeStaffBtnAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func aboutBtnAction(_ sender: UIButton) {
+    }
+}
+
